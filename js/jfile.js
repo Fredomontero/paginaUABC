7fb11561f738d7d5d32cc0789720819cb1a2c3ea
ChristianMontero <christian.montero@uabc.edu.mx>
2017-04-27 20:13:10 -0700

    actualizacion 27-abril

--- js/jfile.js
+++ js/jfile.js
@@ -13,22 +13,22 @@ $(document).ready(function(){
     /*Aqui cambiamos el texto del primer enlace*/
     if(imagen == 'images/noticia1.jpg')
     {
-      $('.titulo_noticia').text("Se llevó a cabo taller de concientización 'Por inclusión y desarrollo, Únete'");
-      $('.texto_noticia').text("El Departamento de Recursos Humanos del Campus Tijuana convocó a los trabajadores  administrativos a un taller de concientización que busca fomentar la inclusión de personas  con discapacidad en la atención...");
+      $('.titulo_noticia').text("Formaliza UABC colaboración con CEITEC (República Checa)");
+      $('.texto_noticia').text("Se concretó convenio de colaboración institucional entre la UABC Campus Tijuana y el  Central European Institute of Technology (CEITEC), República Checa, con el objetivo de  desarrollar...");
     }
 
     /*Aqui cambiamos el texto del segundo enlace*/
     if(imagen == 'images/noticia2.jpg')
     {
-      $('.titulo_noticia').text("Capacitan a alumnos universitarios en metodología Kaizen");
-      $('.texto_noticia').text("La Facultad de Contaduría y Administración, a través de la Coordinación de Administración de  Operaciones, realizaron el Evento Kaizen en la Universidad Autónoma del Campus Tijuana.  El principal...");
+      $('.titulo_noticia').text("Realizan Expo UABC Niños - 2017");
+      $('.texto_noticia').text("Con el objetivo de que los niños conozcan la Universidad y fomentar en ellos la idea de  estudiar una carrera universitaria, se realizó la Expo UABC Niños 2017 en la explanada...");
     }
 
     /*Aqui cambiamos el texto del segundo enlace*/
     if(imagen == 'images/noticia3.jpg')
     {
-      $('.titulo_noticia').text("Inauguran III Congreso Internacional de Educación Artística");
-      $('.texto_noticia').text("Con el objetivo de abordar diversas herramientas, procesos y métodos de creación artística  contemporáneas en función de su distribución en el mercado del arte actual, a partir del  análisis de las...");
+      $('.titulo_noticia').text("Promueven actividad del Centro Europeo de Tecnología (CEITEC-BUT) entre estudiantes");
+      $('.texto_noticia').text("Con el objetivo de proporcionar información general en oportunidades de vinculación y  movilidad académica, así mismo presentar la tecnología de los centros de investigación  europeo se llevó a cabo...");
     }
 
     $('#divImagen').fadeTo(1500, 1.0);
