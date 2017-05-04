3dbd42b3239e2c122a40c01fe191757e57b64f3e
ChristianMontero <christian.montero@uabc.edu.mx>
2017-05-03 23:19:44 -0700

    Actualizacion de noticias

--- js/jfile.js
+++ js/jfile.js
@@ -13,22 +13,22 @@ $(document).ready(function(){
     /*Aqui cambiamos el texto del primer enlace*/
     if(imagen == 'images/noticia1.jpg')
     {
-      $('.titulo_noticia').text("Reconocen a entrenador de Cimarrones UABC por su trabajo en el Mundial Universitario");
-      $('.texto_noticia').text("icardo Fernando Licona Ojeda, entrenador del equipo de futbol americano de Cimarrones  UABC desde 2007, recientemente representó a México en el Mundial Universitario llevado  a cabo en...");
+      $('.titulo_noticia').text("Se llevó a cabo la segunda edición del evento Cimarrones por la niñez");
+      $('.texto_noticia').text("En el marco del 60 aniversario de la máxima casa de estudios, la iniciativa estudiantil “UABC  conecta soluciones”, organizó el evento Cimarrones por la niñez, con el propósito de...");
     }
 
     /*Aqui cambiamos el texto del segundo enlace*/
     if(imagen == 'images/noticia2.jpg')
     {
-      $('.titulo_noticia').text("Realizan Expo UABC Niños - 2017");
-      $('.texto_noticia').text("Con el objetivo de que los niños conozcan la Universidad y fomentar en ellos la idea de  estudiar una carrera universitaria, se realizó la Expo UABC Niños 2017 en la explanada...");
+      $('.titulo_noticia').text("Inauguran Seminario Internacional 'La economía de la nueva frontera ante la nueva realidad'");
+      $('.texto_noticia').text("El doctor Juan Manuel Ocegueda Hernández, Rector de la UABC, inauguró el Seminario  Internacional 'La economía de la nueva frontera ante la nueva realidad', cuyo fin es...");
     }
 
     /*Aqui cambiamos el texto del segundo enlace*/
     if(imagen == 'images/noticia3.jpg')
     {
-      $('.titulo_noticia').text("Promueven actividad del Centro Europeo de Tecnología (CEITEC-BUT) entre estudiantes");
-      $('.texto_noticia').text("Con el objetivo de proporcionar información general en oportunidades de vinculación y  movilidad académica, así mismo presentar la tecnología de los centros de investigación  europeo se llevó a cabo...");
+      $('.titulo_noticia').text("Inauguran XII Jornadas Internacionales “Historia, patrimonio, frontera”");
+      $('.texto_noticia').text("Con el objetivo intercambiar experiencias académicas entre investigadores y alumnos sobre  las problemáticas de la historia y sobre problemáticas actuales del Noroeste de México,  como la migración...");
     }
 
     $('#divImagen').fadeTo(1500, 1.0);
